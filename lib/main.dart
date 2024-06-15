9b393bfcddd93d79cdef6ae87e3fe002081a04e3
kate_sm <smishlyaeva@tut.by>
2024-06-15 16:14:26 +0300

    tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'models/favorites.dart';
+import 'screens/favorites.dart';
+import 'screens/home.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(const TestingApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: HomePage.routeName,
+      builder: (context, state) {
+        return const HomePage();
+      },
+      routes: [
+        GoRoute(
+          path: FavoritesPage.routeName,
+          builder: (context, state) {
+            return const FavoritesPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+class TestingApp extends StatelessWidget {
+  const TestingApp({super.key});
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp.router(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+          ),
+          useMaterial3: true,
+        ),
+        routerConfig: _router,
+      ),
+    );
+  }
 }
